f17ce50905fbbb67afd6b5b2a73960f76db70915
Dory <pemegomachado@gmail.com>
2025-10-21 14:44:32 +0200

    Added F-Pentimino to the Scene

--- main.js
+++ main.js
@@ -14,16 +14,28 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const sqGeometry = new THREE.BoxGeometry(1, 1, 0);
+const fShape = new THREE.Shape().setFromPoints([
+    new THREE.Vector2(0, 0),
+    new THREE.Vector2(0, 1),
+    new THREE.Vector2(-1, 1),
+    new THREE.Vector2(-1, 2),
+    new THREE.Vector2(0, 2),
+    new THREE.Vector2(0, 3),
+    new THREE.Vector2(2, 3),
+    new THREE.Vector2(2, 2),
+    new THREE.Vector2(1, 2),
+    new THREE.Vector2(1, 0),
+    new THREE.Vector2(0, 0)
+]);
+const fGeometry = new THREE.ShapeGeometry(fShape);
+
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const square = new THREE.Mesh(sqGeometry, material);
-scene.add(square);
+const fPentomino = new THREE.Mesh(fGeometry, material);
+scene.add(fPentomino);
 
 camera.position.z = 5;
 
 function animate() {
-    square.rotation.x += 0.01;
-    square.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
