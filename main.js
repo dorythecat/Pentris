7b7c9ec7892502f86a3fd1b0d4c8f00b5a6f37c0
Dory <pemegomachado@gmail.com>
2025-10-21 15:39:58 +0200

    Added I pentomino

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Name of pentominos from https://en.wikipedia.org/wiki/Pentomino
 const fShape = new THREE.Shape().setFromPoints([
     new THREE.Vector2(0, 0),
     new THREE.Vector2(0, 1),
@@ -24,14 +25,31 @@ const fShape = new THREE.Shape().setFromPoints([
     new THREE.Vector2(2, 3),
     new THREE.Vector2(2, 2),
     new THREE.Vector2(1, 2),
-    new THREE.Vector2(1, 0),
-    new THREE.Vector2(0, 0)
+    new THREE.Vector2(1, 0)
 ]);
 const fGeometry = new THREE.ShapeGeometry(fShape);
 
+const iShape = new THREE.Shape().setFromPoints([
+    new THREE.Vector2(0, 0),
+    new THREE.Vector2(0, 4),
+    new THREE.Vector2(1, 4),
+    new THREE.Vector2(1, 0)
+]);
+const iGeometry = new THREE.ShapeGeometry(iShape);
+
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
 const fPentomino = new THREE.Mesh(fGeometry, material);
+const iPentomino = new THREE.Mesh(iGeometry, material);
+
+fPentomino.scale.set(0.1, 0.1, 0.1);
+iPentomino.scale.set(0.1, 0.1, 0.1);
+
+fPentomino.position.x = -2.5;
+iPentomino.position.x = -2.0;
+
 scene.add(fPentomino);
+scene.add(iPentomino);
 
 camera.position.z = 5;
 
