14b4a13dd61eb358acb95a8eededf1021aec8a0c
Dory <pemegomachado@gmail.com>
2025-10-22 09:30:08 +0200

    Added V penntomino

--- main.js
+++ main.js
@@ -93,6 +93,16 @@ const uShape = new THREE.Shape().setFromPoints([
 ]);
 const uGeometry = new THREE.ShapeGeometry(uShape);
 
+const vShape = new THREE.Shape().setFromPoints([
+    new THREE.Vector2(0, 0),
+    new THREE.Vector2(0, 3),
+    new THREE.Vector2(1, 3),
+    new THREE.Vector2(1, 1),
+    new THREE.Vector2(3, 1),
+    new THREE.Vector2(3, 0)
+]);
+const vGeometry = new THREE.ShapeGeometry(vShape);
+
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 const fPentomino = new THREE.Mesh(fGeometry, material);
@@ -102,6 +112,7 @@ const nPentomino = new THREE.Mesh(nGeometry, material);
 const pPentomino = new THREE.Mesh(pGeometry, material);
 const tPentomino = new THREE.Mesh(tGeometry, material);
 const uPentomino = new THREE.Mesh(uGeometry, material);
+const vPentomino = new THREE.Mesh(vGeometry, material);
 
 fPentomino.scale.set(0.1, 0.1, 0.1);
 iPentomino.scale.set(0.1, 0.1, 0.1);
@@ -110,6 +121,7 @@ nPentomino.scale.set(0.1, 0.1, 0.1);
 pPentomino.scale.set(0.1, 0.1, 0.1);
 tPentomino.scale.set(0.1, 0.1, 0.1);
 uPentomino.scale.set(0.1, 0.1, 0.1);
+vPentomino.scale.set(0.1, 0.1, 0.1);
 
 fPentomino.position.x = -2.5;
 iPentomino.position.x = -2.0;
@@ -118,6 +130,7 @@ nPentomino.position.x = -1.0;
 pPentomino.position.x = -0.5;
 tPentomino.position.x = 0.0;
 uPentomino.position.x = 0.5;
+vPentomino.position.x = 1.0;
 
 scene.add(fPentomino);
 scene.add(iPentomino);
@@ -126,6 +139,7 @@ scene.add(nPentomino);
 scene.add(pPentomino);
 scene.add(tPentomino);
 scene.add(uPentomino);
+scene.add(vPentomino);
 
 camera.position.z = 5;
 
